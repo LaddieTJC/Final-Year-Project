941b634e8f46a459add3d5acccce194faa0c10a1
LaddieTJC <103995451+LaddieTJC@users.noreply.github.com>
2023-03-26 13:37:52 +0800

    Delete Flask directory

--- Flask/summary.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import SubmitField,FileField
-from flask_wtf.file import FileAllowed, FileRequired
-
-
-class SummaryForm(FlaskForm):
-    upload = FileField('Please upload a file',validators=[FileRequired(), FileAllowed(['txt','pdf'],'Only pdf and txt files')])
-    submit_btn = SubmitField('Submit')
