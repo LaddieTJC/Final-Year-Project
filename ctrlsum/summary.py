1dc2c87db499d0a22e6a7a2e4043ea94cc199410
LaddieTJC <103995451+LaddieTJC@users.noreply.github.com>
2023-03-26 13:37:25 +0800

    Delete ctrlsum directory

--- ctrlsum/summary.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField,EmailField,SelectField,FileField
-from wtforms.validators import InputRequired, Length, EqualTo, ValidationError
-from flask_wtf.file import FileAllowed, FileRequired
-
-
-class SummaryForm(FlaskForm):
-    upload = FileField('Please upload a file',validators=[FileRequired(), FileAllowed(['txt','pdf'],'Only pdf and txt files')])
-    submit_btn = SubmitField('Submit')
